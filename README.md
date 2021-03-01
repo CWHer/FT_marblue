99bfe661ac5e36f77e5436cb7c36719a2bd33563
cwher <cwher@outlook.com>
2021-03-01 19:39:53 +0800

    feat: add ftplot

--- README.md
+++ README.md
@@ -6,7 +6,15 @@
 
 ### 参考资料
 
-[_手把手教你用傅立叶变换画可达鸭_](https://zhuanlan.zhihu.com/p/72632360)
+1. [_手把手教你用傅立叶变换画可达鸭_](https://zhuanlan.zhihu.com/p/72632360)
+
+2. [_matplotlib：先搞明白plt. /ax./ fig再画_](https://zhuanlan.zhihu.com/p/93423829)
+
+3. [_Matplotlib_](https://www.matplotlib.org.cn/)
+
+4. [_Python+Matplotlib制作动画_](https://www.cnblogs.com/endlesscoding/p/10308111.html)
+
+5. [_Delaunay剖分_](http://www.geom.uiuc.edu/~samuelp/del_project.html)
 
 
 
@@ -32,8 +40,30 @@ MST是Delaunay剖分的子图：
 
 ​	如果(u,v)不在Delaunay剖分，则(u,v)外接圆内含有其它点，不妨w，从而(u,v)为u-v-w里的最长边
 
+在Delaunay剖分的基础上使用$Kruskal$即可
+
 
 
 
 
 #### 5.Fourier变换画图
+
+离散Fourier变换
+$$
+X_k = \sum_{n=0}^{N-1} x_n\cdot e^{-\frac {i 2\pi}{N}kn} = \sum_{n=0}^{N-1} x_n\cdot [\cos(2 \pi k n / N) - i\cdot \sin(2 \pi k n / N)]
+$$
+
+$$
+x_n = \frac{1}{N} \sum_{k=0}^{N-1} X_k\cdot e^{i 2 \pi k n / N}
+$$
+
+以$n=8$为例
+$$
+x_1 = \frac{1}{8}(X_0\cdot e^{0} + X_1\cdot e^{i2\pi/8} + X_2\cdot e^{{i4\pi/8}} + X_3\cdot e^{{i6\pi/8}} + X_4\cdot e^{{i8\pi/8}} + X_5\cdot e^{i10\pi/8} + X_6\cdot e^{i12\pi/8} + X_7\cdot e^{i14\pi/8})
+$$
+相当于让 $X_0$ 不动，在此基础上$X_1$逆时针转45度，以此类推。另外，圆的半径就是$\frac{|X_i|}{N}$
+
+这些圆如果首尾相接，第二个圆心定在第一个圆的向量末端……，那么最后一个圆的向量指向的位置就是所有向量的和，即为$x_i$
+
+使用`matplotlib`里的`FuncAnimation`，进行绘制。先预处理出每个圆心在每一帧的位置，然后在帧之间重新渲染圆心和圆之间的连线并加入新的点。
+
---
+++ ftplot.py
@@ -0,0 +1,84 @@
+# plot image after Fourier transform
+#   using matplotlib.animation
+#   passing one point each frame
+# input: list of traverse points
+#        2xn np.array
+# output: animation
+import numpy as np
+from scipy.fftpack import fft
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+
+
+class FourierTransPlot():
+    def __init__(self, x: np.array):
+        # x -> X
+        # regular x into [0,1]x[0,1]
+        x = x / x.max()
+        self.X = fft(x[0, :] + x[1, :] * 1j)
+        # let frame_number = circle_number
+        #   such that it passes one point each frame
+        self.number = len(self.X)
+        self.__generatePos()
+
+    # remove relatively small X
+    # TBD
+    def __remove(self):
+        pass
+
+    def __generatePos(self):
+        # circle i at different frame t
+        # X[i][t] = self.X[i] * exp(2*pi*1j*t*i/n) / n
+        X = []
+        for i in range(self.number):
+            X.append(self.X[i] * np.exp(
+                2 * np.pi * 1j * i * np.arange(self.number) / self.number) /
+                     self.number)
+        # pos[i][t] is the position of circle i's center at t frame
+        # pos[i][t] = pos[i-1][t] + X[i][t]
+        # len(pos) = t+1
+        self.pos = [[0 for i in range(self.number)]]
+        for i in range(self.number):
+            self.pos.append(self.pos[-1] + X[i])
+
+    def plot(self):
+        fig, ax = plt.subplots()
+        ax.set(xlim=[0, 1], ylim=[0, 1])
+        ax.axis('off')
+        # initialize lines
+        lines = [ax.plot([], [], alpha=0.3)[0] for i in range(self.number)]
+        # initialize circles
+        circles = []
+        for i in range(self.number):
+            radius = np.abs(self.X[i] / self.number)
+            # temporary place
+            circle = Circle((0, 0), radius, alpha=0.3, fill=False)
+            circles.append(circle)
+            ax.add_artist(circle)
+
+        path = ax.plot([], [])[0]
+        path_x = []
+        path_y = []
+
+        def render_frame(k: int):
+            # render lines between two circles
+            for i in range(self.number):
+                u, v = self.pos[i][k], self.pos[i + 1][k]
+                lines[i].set_data([u.real, v.real], [u.imag, v.imag])
+            # render circles with new center
+            for i in range(self.number):
+                u = self.pos[i][k]
+                circles[i].center = (u.real, u.imag)
+            # add new point
+            u = self.pos[-1][k]
+            path_x.append(u.real)
+            path_y.append(u.imag)
+            path.set_data(path_x, path_y)
+
+        animation = FuncAnimation(fig,
+                                  render_frame,
+                                  frames=self.number,
+                                  interval=100)
+        # animation.save("marblue.gif")
+        plt.show()
